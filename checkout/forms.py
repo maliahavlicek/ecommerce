b4c6e13afd0621636eb71d4e7b618f4f3ebb4daa
maliahavlicek <malia.havlicek@gmail.com>
2020-04-19 20:20:26 -0600

    get the modelForm to show up

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,7 +7,7 @@ year = int(datetime.now().strftime("%Y"))
 
 class MakePaymentForm(forms.Form):
     MONTH_CHOICES = [(i, i) for i in range(1, 12)]
-    YEAR_CHOICES = [(i, i) for i in range(2020, 2040)]
+    YEAR_CHOICES = [(i, i) for i in range(year, year+20)]
 
     credit_card_number = forms.CharField(label='Credit Card Number', required=False)
     ccv = forms.CharField(label="Security Code", required=False)
@@ -16,12 +16,12 @@ class MakePaymentForm(forms.Form):
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
 
-class OrderForm(forms.Form):
+class OrderForm(forms.ModelForm):
 
     class Meta:
         model = Order
-        fields = (
+        fields = [
             'full_name', 'phone_number', 'country', 'postcode',
             'town_or_city', 'street_address1', 'street_address2',
             'county'
-        )
+        ]
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from os import environ
-# import env
+import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
